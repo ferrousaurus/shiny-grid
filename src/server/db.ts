042e6db60ec2c2f8cdee7e8aa273b9d81fa90402
OxidalWave <github@oxidalwave.com>
2026-07-05 21:11:52 -0500

    Use pg.Pool directly

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,11 +1,8 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 import { PrismaClient } from "../../prisma/generated/client.ts";
-import { env } from "../env.ts";
 
-const pool = new pg.Pool({
-  connectionString: env.DATABASE_URL,
-});
+const pool = new pg.Pool();
 
 const adapter = new PrismaPg(pool);
 
